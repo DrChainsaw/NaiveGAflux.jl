a7f2cbcb27ee4ebdd2d6a3806f0ee88826c45af6
DrChainsaw <christian.kyril.skarby@gmail.com>
2020-06-01 21:26:16 +0200

    Set create_edge strat in crossoverswap to PostAlignJuMP Apply the mutation after crossoverswap

--- test/crossover.jl
+++ test/crossover.jl
@@ -6,11 +6,11 @@
             vc1, i1, o1 = stripvertex(v1)
             vc2, i2, o2 = stripvertex(v2)
 
-            foreach(iv -> create_edge!(iv, vc2), i1)
-            foreach(ov -> create_edge!(vc2, ov), o1)
+            foreach(iv -> create_edge!(iv, vc2, strategy = PostAlignJuMP()), i1)
+            foreach(ov -> create_edge!(vc2, ov, strategy = PostAlignJuMP()), o1)
 
-            foreach(iv -> create_edge!(iv, vc1), i2)
-            foreach(ov -> create_edge!(vc1, ov), o2)
+            foreach(iv -> create_edge!(iv, vc1, strategy = PostAlignJuMP()), i2)
+            foreach(ov -> create_edge!(vc1, ov, strategy = PostAlignJuMP()), o2)
             return vc1, vc2
         end
 
@@ -18,8 +18,7 @@
             i, o = copy(inputs(v)), copy(outputs(v))
             foreach(iv -> remove_edge!(iv, v; strategy = NoSizeChange()), i)
             foreach(ov -> remove_edge!(v, ov, strategy = NoSizeChange()), o)
-            vc = clone(v)
-            return vc, i, o
+            return v, i, o
         end
 
         @testset "Simple Dense swap" begin
@@ -38,9 +37,14 @@
             gb = g(1:3, "b", dv)
 
             crossoverswap(vertices(ga)[3], vertices(gb)[3])
+            apply_mutation(ga)
+            apply_mutation(gb)
 
             @test nout.(vertices(ga)) == [3,4,2,6]
             @test nout.(vertices(gb)) == [3,1,5,3]
+
+            @test size(ga(ones(3, 2))) == (6, 2)
+            @test size(gb(ones(3, 2))) == (3, 2)
         end
     end
 end
