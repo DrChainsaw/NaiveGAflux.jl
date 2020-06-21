43c4847619d9dac0518b452b3cc0e52e7c5915e7
DrChainsaw <christian.kyril.skarby@gmail.com>
2020-06-21 22:20:39 +0200

    Remove todo about traits for dummy vertices

--- src/crossover.jl
+++ src/crossover.jl
@@ -302,10 +302,11 @@ function stripinedges!(v)
 end
 
 
-# TODO: All dummyvertices can be invariantvertex?
 dummyvertex(v) = dummyvertex(v, trait(v))
 dummyvertex(v, t::DecoratingTrait) = dummyvertex(v, base(t))
+# Invariant vertex is needed to make swapping succeed more often with multiple inputs. When I wrote this code I knew why, but at the time of writing this comment I didn't :(
 dummyvertex(v, t) = invariantvertex(ActivationContribution(identity), v; traitdecoration = named("$(name(v)).dummy"))
+# Not sure why, but using single input conc here appears to have less drastic effects on sizes when swapping
 dummyvertex(v, t::SizeAbsorb) = conc(v; dims=1, traitdecoration = named("$(name(v)).dummy"), outwrap = ActivationContribution)
 
 function addinedges!(v, ins, strat = default_crossoverswap_strategy)
