daf45c708199bb84f1a36dec846d524fc70219b7
DrChainsaw <Christian.kyril.skarby@gmail.com>
2021-03-30 23:34:58 +0200

    Fix candidate tests

--- test/candidate.jl
+++ test/candidate.jl
@@ -1,91 +1,60 @@
 @testset "Candidate" begin
 
     struct DummyFitness <: AbstractFitness end
-    import NaiveGAflux: FileCandidate
+    NaiveGAflux.fitness(::DummyFitness, f, gen) = 17
+    import NaiveGAflux: FileCandidate, AbstractWrappingCandidate, opt
     import MemPool
-    @testset "CandidateModel $wrp" for wrp in (
-        identity,
-        HostCandidate,
-        CacheCandidate,
-        FileCandidate,
-        CacheCandidate ∘ HostCandidate,
-        CacheCandidate ∘ FileCandidate ∘ HostCandidate)
+    @testset "$ctype" for (ctype, candfun) in (
+        (CandidateModel, CandidateModel),
+        (CandidateOptModel, g -> CandidateOptModel(g, Descent(0.01)))
+    )
+    
+        @testset " $wrp" for wrp in (
+            identity,
+            FileCandidate)
 
+            invertex = inputvertex("in", 3, FluxDense())
+            hlayer = mutable("hlayer", Dense(3,4), invertex)
+            outlayer = mutable("outlayer", Dense(4, 2), hlayer)
+            graph = CompGraph(invertex, outlayer)
 
-        import NaiveGAflux: AbstractFunLabel, Train, TrainLoss, Validate
+            try
 
-        invertex = inputvertex("in", 3, FluxDense())
-        hlayer = mutable("hlayer", Dense(3,4), invertex)
-        outlayer = mutable("outlayer", Dense(4, 2), hlayer)
-        graph = CompGraph(invertex, outlayer)
+                cand = wrp(candfun(graph))
 
-        try
+                @test fitness(cand, DummyFitness(), 0) == 17
 
-            cand = wrp(CandidateModel(graph, Descent(0.01), (x,y) -> sum(x .- y), DummyFitness()))
+                graphmutation = VertexMutation(MutationFilter(v -> name(v)=="hlayer", AddVertexMutation(ArchSpace(DenseSpace([1], [relu])))))
+                optmutation = OptimizerMutation((Momentum, Nesterov, ADAM))
+                evofun = evolvemodel(graphmutation, optmutation)
+                newcand = evofun(cand)
 
-            labs = []
-            function NaiveGAflux.instrument(l::AbstractFunLabel, s::DummyFitness, f)
-                push!(labs, l)
-                return f
+                @test NaiveGAflux.graph(newcand, nv) == 4
+                @test NaiveGAflux.graph(cand, nv) == 3
+
+                optimizer(c) = typeof(opt(c)) 
+
+                if ctype === CandidateModel
+                    @test optimizer(newcand) === optimizer(cand) === Nothing
+                else
+                    @test optimizer(newcand) !== optimizer(cand)
+                end
+
+                teststrat() = NaiveGAflux.default_crossoverswap_strategy(v -> ones(nout_org(v)))
+                graphcrossover = VertexCrossover(CrossoverSwap(;pairgen = (v1,v2) -> (1,1), strategy=teststrat); pairgen = (v1,v2;ind1) -> ind1==1 ? (2,3) : nothing)
+                optcrossover = OptimizerCrossover()
+                crossfun = evolvemodel(graphcrossover, optcrossover)
+
+                newcand1, newcand2 = crossfun((cand, newcand))
+
+                @test optimizer(newcand1) === optimizer(newcand)
+                @test optimizer(newcand2) === optimizer(cand)
+
+                @test NaiveGAflux.graph(newcand1, nv) == 4
+                @test NaiveGAflux.graph(newcand2, nv) == 3
+            finally
+                MemPool.cleanup()
             end
-
-            data(c::CandidateModel) = (ones(Float32, 3, 2), ones(Float32, 2,2))
-            data(c::HostCandidate) = gpu.(data(c.c))
-            data(c::FileCandidate) = NaiveGAflux.callcand(data, c)
-            data(c::AbstractCandidate) = data(c.c)
-
-            Flux.train!(cand, data(cand))
-
-            @test labs == [Train(), TrainLoss()]
-
-            NaiveGAflux.fitness(::DummyFitness, f) = 17
-            @test fitness(cand) == 17
-
-            @test labs == [Train(), TrainLoss(), Validate()]
-
-            wasreset = false
-            NaiveGAflux.reset!(::DummyFitness) = wasreset = true
-
-            reset!(cand)
-
-            @test wasreset
-
-
-            graphmutation = VertexMutation(MutationFilter(v -> name(v)=="hlayer", AddVertexMutation(ArchSpace(DenseSpace([1], [relu])))))
-            optmutation = OptimizerMutation((Momentum, Nesterov, ADAM))
-            evofun = evolvemodel(graphmutation, optmutation)
-            newcand = evofun(cand)
-
-            @test NaiveGAflux.graph(newcand, nv) == 4
-            @test NaiveGAflux.graph(cand, nv) == 3
-
-            optimizer(c::AbstractCandidate) = optimizer(c.c)
-            optimizer(c::CandidateModel) = typeof(c.opt)
-            optimizer(c::FileCandidate) = NaiveGAflux.callcand(optimizer, c)
-
-            @test optimizer(newcand) != optimizer(cand)
-
-            Flux.train!(cand, data(cand))
-            Flux.train!(newcand, data(newcand))
-
-            teststrat() = NaiveGAflux.default_crossoverswap_strategy(v -> ones(nout_org(v)))
-            graphcrossover = VertexCrossover(CrossoverSwap(;pairgen = (v1,v2) -> (1,1), strategy=teststrat); pairgen = (v1,v2;ind1) -> ind1==1 ? (2,3) : nothing)
-            optcrossover = OptimizerCrossover()
-            crossfun = evolvemodel(graphcrossover, optcrossover)
-
-            newcand1, newcand2 = crossfun((cand, newcand))
-
-            @test optimizer(newcand1) == optimizer(newcand)
-            @test optimizer(newcand2) == optimizer(cand)
-
-            @test NaiveGAflux.graph(newcand1, nv) == 4
-            @test NaiveGAflux.graph(newcand2, nv) == 3
-
-            Flux.train!(newcand1, data(newcand1))
-            Flux.train!(newcand2, data(newcand2))
-
-        finally
-            MemPool.cleanup()
         end
     end
 
@@ -107,7 +76,7 @@
 
             @testset "Functor" begin
                 v1 = mutable("v1", Dense(3,3;init=idmapping), inputvertex("in", 3, FluxDense()))
-                cand1 = FileCandidate(CandidateModel(CompGraph(inputs(v1)[], v1), Descent(0.01), (x,y) -> sum(x .- y), DummyFitness()))
+                cand1 = FileCandidate(CandidateModel(CompGraph(inputs(v1)[], v1)))
 
                 mul(x::AbstractArray) = 2 .* x
                 mul(x) = x
@@ -122,7 +91,7 @@
 
 
                 v = mutable("v1", Dense(3,3), inputvertex("in", 3, FluxDense()))
-                secand = CacheCandidate(FileCandidate(CandidateModel(CompGraph(inputs(v)[], v), Descent(0.01), (x,y) -> sum(x .- y), DummyFitness())))
+                secand = FileCandidate(CandidateModel(CompGraph(inputs(v)[], v)))
 
                 indata = randn(3, 2)
                 expected = v(indata)
@@ -176,7 +145,7 @@
 
         @testset "Global learning rate scaling" begin
             v1 = inputvertex("in", 3, FluxDense())
-            pop = CandidateModel.(Ref(CompGraph(v1, v1)), Descent.(0.1:0.1:1.0), Flux.Losses.mse, Ref(DummyFitness()))
+            pop = CandidateOptModel.(Ref(CompGraph(v1, v1)), Descent.(0.1:0.1:1.0))
 
             lr(c) = c.opt.eta
             @test lr.(pop) == 0.1:0.1:1.0
